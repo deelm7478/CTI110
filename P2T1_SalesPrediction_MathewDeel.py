e64588405812c5c018c6b96522859ea87efdbb22
deelm7478 <43555442+deelm7478@users.noreply.github.com>
2018-09-24 17:39:22 -0400

    Add files via upload
    Python 2 Tutorial 1

---
+++ P2T1_SalesPrediction_MathewDeel.py
@@ -0,0 +1,9 @@
+# Calculates the profit made based on the total sales
+# 9/24/18
+# CTI-110 P2T1 - Sales Prediction
+# Mathew Deel
+#
+
+sales = float(input("Enter the projected amount of total sales:"))
+profit = int(sales * 0.23)
+print("The profit that will be made from that amount is", profit,"dollars.")
